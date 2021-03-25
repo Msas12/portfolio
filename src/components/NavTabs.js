70dce9d51ac6f3dce63ec4b47e32b774012a3798
Mike Sasiela <m5455y@gmail.com>
2021-03-24 20:26:10 -0500

    have main page and navbar set up

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Navbar, Nav,} from 'react-bootstrap'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -13,12 +15,16 @@ const location = useLocation();
   return (
     <Navbar bg="dark" expand="lg">
       <Navbar.Brand as={Link} to="/"><img src="imgs/memoji.png" alt="memoji icon" width="50" height="50"/> Mike Sasiela</Navbar.Brand>
+
+      {/* Dark Mode Toggle */}
+
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link as={Link} to="/" className= {location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</Nav.Link>
           <Nav.Link as={Link} to="/projects" className={location.pathname === "/projects" ? "nav-link active" : "nav-link"}>Projects</Nav.Link>
           <Nav.Link as={Link} to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contacts</Nav.Link>
+          <Nav.Link href="./Michael Sasiela Resume 2021.pdf">Resume</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,23 +1,51 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/fontawesome-free-solid'
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithubSquare, faInstagramSquare, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 
 
 function Home() {
   return (
-    <div>
-      <FontAwesomeIcon icon={faCoffee}/>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
+<main>
+        <div className="card-group">
+            <div className="mb-3 mt-5 mx-auto" style={{maxWidth: '90%'}}>
+                <div className="row">
+                    <div className="col-md-4">
+                        <img className="me-image" src="imgs/me5.jpeg" alt="Mike" style= {{width: "100%"}}/>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h4 className="card-title">Mike Sasiela</h4>
+                                <h5 className="card-text">Full Stack Developer</h5>
+
+                                <p className="card-text">I have had a love for the inner-workings of tech my whole life, and
+                                    after working in the Information and Technology field for a few years I knew I had
+                                    to take it to the next level.</p>
+                                <p className="card-text">I am currently enrolled in a Coding Bootcamp at Washington University in St. Louis -
+                                    McKelvey School of Engineering.</p>
+                                <p className="card-text">Upon graduation of this 24 week intensive Bootcamp, I will earn a Certificate of
+                                    completion with competencies in: HTML5, CSS3, JavaScript, jQuery, Express.js,
+                                    React.js, Node.js, Progressive Web Apps, Database Theory, MongoDB, MySQL, Git,
+                                    Python, Java, C#, PHP and more.</p>
+      
+                                <h4 className="card-text">Download My Resume: &nbsp;<a
+                                        href="./Michael Sasiela Resume 2021.pdf" download="Mike Sasiela Resume"
+                                        target="_blank" rel="noreferrer"><FontAwesomeIcon className="fas" icon={faFilePdf}/></a></h4>
+                            </div>
+
+                            <div className="container footer text-center">
+                                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/michael-sasiela-04121992/"><FontAwesomeIcon className="p-1 fab fa-4x" icon={faLinkedin} /></a>
+                                    <a target="_blank" rel="noreferrer" href="https://github.com/Msas12"><FontAwesomeIcon className="p-1 fab fa-4x" icon={faGithubSquare} /></a>
+                                    <a target="_blank" rel="noreferrer" href="https://www.instagram.com/msasiela/"><FontAwesomeIcon className="p-1 fab fa-4x" icon={faInstagramSquare} /></a>
+                                    <a target="_blank" rel="noreferrer" href="https://www.facebook.com/mike.sasiela"><FontAwesomeIcon className="p-1 fab fa-4x" icon={faFacebookSquare} /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
   );
 }
 
